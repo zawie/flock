df0e907c653a6454c29716c619b90849d3b0beb2
Adam Zawierucha <zawiedev@gmail.com>
2019-10-11 15:22:21 -0500

    Initial vector class

---
+++ vector2.js
@@ -0,0 +1,13 @@
+class Vector2 {
+    constructor (a,b) {
+        this.x = a
+        this.y = b
+    }
+    unit(){
+        s = pow(a,2) + pow(b,2) + pow(c,2)
+        return pow(s,1/2)
+    }
+}
+
+pos = new Vector2(1,2)
+print(pos)
